2b1a49a2c40942f83b5303fe6a7e3f24f7dc8f34
GTYang <gtq.yang@gmail.com>
2025-11-05 17:15:19 +0800

    commit

--- app/src/main/java/com/example/myviewjava/MainActivity.java
+++ app/src/main/java/com/example/myviewjava/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.myviewjava;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // 輸入框功能
+        EditText myEditText = findViewById(R.id.edit_text_id);
+        myEditText.requestFocus();
+        String inputText = myEditText.getText().toString();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
