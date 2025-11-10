e2796b1639b28efa2ee658a733817b862d04fa63
GTYang <gtq.yang@gmail.com>
2025-11-10 17:24:55 +0800

    commit

--- app/src/main/java/com/example/myviewjava/MainActivity.java
+++ app/src/main/java/com/example/myviewjava/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.myviewjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "Button clicked!",
+                        Toast.LENGTH_SHORT).show();
+
+                // 在這裡處理按鈕點擊事件;
+            }
         });
     }
 }
